d09696225855b9b66011592cdfa87b853199c4a9
Raquel Dionísio <dionisio@stadtwerk.org>
2022-09-02 13:11:22 +0200

    Added descritption comments to interface BaseItem

--- source/items/item.interface.ts
+++ source/items/item.interface.ts
@@ -1,12 +1,11 @@
 // source/items/item.interface.ts
 
 export interface BaseItem {
-    name: string,
-    price: number,
-    description: string,
-    image: string,
+    name: string, //  Unique identifier for the item record
+    price: number, // Name of the item
+    description: string, // Description of the item
+    image: string, // URL pointing to the item's image
 }
-
 export interface Item extends BaseItem {
     id: number
 }
