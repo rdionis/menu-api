65567b6f53a904a510449d9e573113fa80f877c1
Raquel Dionísio <dionisio@stadtwerk.org>
2022-09-05 19:48:58 +0100

    Adding not found middleware

--- source/index.ts
+++ source/index.ts
@@ -6,7 +6,9 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import { itemsRouter } from './items/items.router'
+import { itemsRouter } from './items/items.router';
+import { errorHandler } from './middleware/error.middleware';
+import { notFoundHandler } from './middleware/not-found.middleware';
 
 
 dotenv.config()
@@ -32,6 +34,11 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use('/api/menu/items', itemsRouter);
+
+app.use(errorHandler);
+app.use(notFoundHandler); // this creates a catch-all handler for your app
+
+
 
 // Setting up first route
 
